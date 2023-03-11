a6c1693c2fe653769081f50f57f5449ba892f316
Ryo <sakair0903@gmail.com>
2023-03-11 00:46:52 -0800

    adding and listing campaigns

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   console.log("currentAccount: ", currentAccount);
 
-  const contractAddress = "0x31BD57F6290656a5f3fb74F3CF6319aBe7e7C6a5"; //Goerli Test Network address of my app
+  const contractAddress = "0x3295338611A8E5A344B925C88423D4811e1025Bb"; //Goerli Test Network address of my app
   const contractABI = abi.abi;
   // window.ethereumにアクセスできることを確認します。
   const checkIfWalletIsConnected = async () => {
@@ -25,6 +25,7 @@ const App = () => {
       }
       // ユーザーのウォレットへのアクセスが許可されているかどうかを確認します。
       const accounts = await ethereum.request({ method: "eth_accounts" });
+      getAllCampaigns();
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
@@ -93,6 +94,79 @@ const App = () => {
     checkIfWalletIsConnected();
   }, []);
 
+  const [allCampaigns, setAllCampaigns] = useState([]);
+
+  const getAllCampaigns = async () => {
+    const { ethereum } = window;
+
+    try {
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const fundPortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        /* コントラクトからgetAllWavesメソッドを呼び出す */
+        const campaigns = await fundPortalContract.getAllCampaigns();
+        /* UIに必要なのは、アドレス、タイムスタンプ、メッセージだけなので、以下のように設定 */
+        const campaignsCleaned = campaigns.map((campaign) => {
+          return {
+            fundingGoal: campaign.fundingGoal,
+            deadline: campaign.deadline,
+            beneficiary: campaign.beneficiary,
+          };
+        });
+
+        /* React Stateにデータを格納する */
+        setAllCampaigns(campaignsCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * `emit`されたイベントに反応する
+   */
+  useEffect(() => {
+    let fundPortalContract;
+
+    const onNewCampaign = (fundingGoal, deadline, beneficiary) => {
+      console.log("NewCampaign", fundingGoal, deadline, beneficiary);
+      setAllCampaigns((prevState) => [
+        ...prevState,
+        {
+          fundingGoal: fundingGoal,
+          deadline: deadline,
+          beneficiary: beneficiary,
+        },
+      ]);
+    };
+
+    /* NewWaveイベントがコントラクトから発信されたときに、情報を受け取ります */
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      fundPortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      fundPortalContract.on("CampaignCreated", onNewCampaign);
+    }
+    /*メモリリークを防ぐために、NewWaveのイベントを解除します*/
+    return () => {
+      if (fundPortalContract) {
+        fundPortalContract.off("CampaignCreated", onNewCampaign);
+      }
+    };
+  }, []);
+
   const [items, setItems] = useState([]);
 
   function addItem(inputText) {
@@ -108,6 +182,22 @@ const App = () => {
         createCampaign={createCampaign}
         currentAccount={currentAccount}
       />
+      <h2>All Campaigns</h2>
+      <ul>
+        {allCampaigns.map((campaign, index) => (
+          <li key={index}>
+            <div>
+              <strong>Funding Goal:</strong> {campaign.fundingGoal.toString()}
+            </div>
+            <div>
+              <strong>Deadline:</strong> {campaign.deadline.toString()}
+            </div>
+            <div>
+              <strong>Beneficiary:</strong> {campaign.beneficiary}
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
